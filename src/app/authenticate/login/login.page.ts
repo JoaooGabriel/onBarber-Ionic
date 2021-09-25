96081eb2b5bd9f738a730a4d6ce0e239b31826f0
JoaooGabriel <caetanojoaogabriel@gmail.com>
2021-09-25 16:47:31 -0300

    refactor: receiving input data, email and password, validating the login method

--- src/app/authenticate/login/login.page.ts
+++ src/app/authenticate/login/login.page.ts
@@ -16,6 +16,7 @@ export class LoginPage implements OnInit {
   constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.user = this.userService.init();
   }
 
   public isPasswordVisible(state: boolean) {
@@ -32,26 +33,15 @@ export class LoginPage implements OnInit {
     }
   }
 
-  setEmail($event) {
-    // console.log($event.target.value);
-    this.user.email = $event.target.value;
-  }
-
-  setPassword($event) {
-    // console.log($event.target.value);
-    this.user.password = $event.target.value;
-  }
-
   login() {
-    console.log(this.user);
+    const email = this.user.email;
+    const password = this.user.password;
+    const result = this.userService.login(email, password);
 
-    // const email = this.user.email;
-    // const password = this.user.password;
+    if (!result) {
+      throw Error('E-mail ou senha inválidos!');
+    }
 
-    // const result = this.userService.login(email, password);
-
-    // if (!result) {
-    //   throw Error('E-mail ou senha inválidos!');
-    // }
+    alert('Autenticado com sucesso!');
   }
 }
