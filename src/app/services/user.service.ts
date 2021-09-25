5a6dedf2b12b60141f12392d15a221d9b2db9b4a
JoaooGabriel <caetanojoaogabriel@gmail.com>
2021-09-25 16:50:14 -0300

    feat: create init method and user mock

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@angular/core';
 
-// export interface IUserLogin {
-//   email: string;
-//   password: string;
-// }
-
 export interface IUser {
   id: string;
   name: string;
@@ -17,21 +12,33 @@ export interface IUser {
   providedIn: 'root'
 })
 export class UserService {
-  private users: IUser[] = [];
+  private users: IUser[] = [
+    {
+      id: 'ff8feb01-76c3-41dd-a1ae-9933974ed7b4',
+      name: 'Test',
+      email: 'test.email@gmail.com',
+      phone: '+5511999999999',
+      password: 'test1234'
+    }
+  ];
   constructor() { }
+
+  public init(): IUser {
+    return {
+      id: '',
+      name: '',
+      email: '',
+      phone: '',
+      password: ''
+    };
+  }
 
   public find(userId: string): IUser {
     return { ...this.users.find((u) => u.id === userId)};
   }
 
   public login(email: string, password: string): IUser {
-    const user = { ...this.users.find((u) => u.email === email && u.password === password)};
-
-    if (!user) {
-      return undefined;
-    }
-
-    user.password = undefined;
+    const user = this.users.find((u) => u.email === email && u.password === password);
 
     return user;
   }
