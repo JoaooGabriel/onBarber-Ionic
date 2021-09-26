bfb1053afff56658db050403aed4786b2a870e4b
fsvboas <felippesantosvb@hotmail.com>
2021-09-25 22:47:42 -0300

    refactor: fixed tab button

--- src/app/authenticate/authenticate-routing.module.ts
+++ src/app/authenticate/authenticate-routing.module.ts
@@ -7,16 +7,25 @@ const routes: Routes = [
   {
     path: '',
     component: AuthenticatePage,
-  },
-  {
-    path: 'login',
-    loadChildren: () =>
-      import('./login/login.module').then((m) => m.LoginPageModule),
-  },
-  {
-    path: 'register',
-    loadChildren: () =>
-      import('./register/register.module').then((m) => m.RegisterPageModule),
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('./login/login.module').then((m) => m.LoginPageModule),
+      },
+      {
+        path: 'register',
+        loadChildren: () =>
+          import('./register/register.module').then(
+            (m) => m.RegisterPageModule
+          ),
+      },
+    ],
   },
 ];
 
