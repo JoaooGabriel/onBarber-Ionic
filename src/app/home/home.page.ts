6e8dc91999103a990dbc2c9682e135fa10912e7d
JoaooGabriel <caetanojoaogabriel@gmail.com>
2021-09-26 22:04:05 -0300

    feat: added logout and getSchedules method and schedules mock

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { IUser } from './../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public user: IUser;
+  public schedules = [
+    {
+      day: '26/09/2021',
+      hour: '13:00',
+      barberName: 'João'
+    },
+    {
+      day: '29/09/2021',
+      hour: '12:00',
+      barberName: 'Samuka'
+    },
+    {
+      day: '07/10/2021',
+      hour: '17:00',
+      barberName: 'Tiago'
+    },
+    {
+      day: '20/10/2021',
+      hour: '09:00',
+      barberName: 'Bruno'
+    }
+  ];
 
-  constructor() {}
+  constructor(private userService: UserService) {
+    this.getLoggedUser();
+  }
 
+  public getLoggedUser() {
+    const user = this.userService.findLoggedUser();
+
+    this.user = user;
+  }
+
+  public getSchedules() {
+    return this.schedules;
+  }
+
+  logout() {
+    this.userService.logout();
+  }
 }
