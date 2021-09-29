b2435ed7c6523f3f8c0723ceb477269ff2cea4d2
brunobnh21 <brunohori@hotmail.com>
2021-09-28 21:21:22 -0300

    refactor: remove schedule mock and added findAll and delete method of schedule services

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ISchedule, ScheduleService } from '../services/schedule.service';
 import { UserService } from '../services/user.service';
 import { IUser } from './../services/user.service';
 
@@ -9,35 +10,13 @@ import { IUser } from './../services/user.service';
 })
 export class HomePage {
   public user: IUser;
-  public schedules = [
-    {
-      id: '3f4f8177-b9db-45ad-9bf8-16297ccc0e51',
-      day: '26/09/2021',
-      hour: '13:00',
-      barberName: 'João'
-    },
-    {
-      id: '124ff50c-3881-47fa-ae18-cb2fbf647e64',
-      day: '29/09/2021',
-      hour: '12:00',
-      barberName: 'Samuka'
-    },
-    {
-      id: 'd5eb8ca8-4582-43e8-aa16-e30d3242c261',
-      day: '07/10/2021',
-      hour: '17:00',
-      barberName: 'Tiago'
-    },
-    {
-      id: '6fc05cac-2993-43b2-a5a3-bfa94353abc7',
-      day: '20/10/2021',
-      hour: '09:00',
-      barberName: 'Bruno'
-    }
-  ];
+  public schedules: ISchedule[] = this.findAll();
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private scheduleService: ScheduleService) {
     this.getLoggedUser();
+  }
+  public findAll(){
+    return this.scheduleService.findAll();
   }
 
   public getLoggedUser() {
@@ -54,17 +33,8 @@ export class HomePage {
     this.userService.logout();
   }
 
-  delete(scheduleId: string) {
-    if (this.schedules.length === 1) {
-      this.schedules = [];
-    }
-
-    const indexSchedules = this.schedules.findIndex((schedule) => schedule.id === scheduleId );
-
-    if (indexSchedules > -1) {
-      this.schedules.splice(indexSchedules, 1);
-    }
-
-    throw Error('Ocorreu um erro ao executar a operação!');
+  public delete(scheduleId:string){
+    const schedules = this.scheduleService.delete(scheduleId);
+    this.schedules = schedules;
   }
 }
