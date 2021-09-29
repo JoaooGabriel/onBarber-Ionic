e3a5360a9a1cc67674a5dadf30f6eae1d4f8e67c
brunobnh21 <brunohori@hotmail.com>
2021-09-28 21:18:35 -0300

    feat: create getBarberName method and added create and goHome method of schedule services

--- src/app/home/schedule/schedule.page.ts
+++ src/app/home/schedule/schedule.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {v4 as uuid} from 'uuid';
+import { ISchedule, ScheduleService } from 'src/app/services/schedule.service';
+
+
 
 @Component({
   selector: 'app-schedule',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SchedulePage implements OnInit {
 
-  constructor() { }
+  private schedule:ISchedule;
+
+  constructor(private scheduleService:ScheduleService) { }
 
   ngOnInit() {
+    this.schedule = this.scheduleService.init();
   }
 
+  public getBarberName(barberName: string){
+    this.schedule.barberName = barberName;
+  }
+
+  public create(){
+    if(!this.schedule.day || !this.schedule.hour || !this.schedule.barberName ){
+      alert('Por favor, preencha todos os campos ou selecione um barbeiro!');       
+      throw Error('Por favor, preencha todos os campos ou selecione um barbeiro!');
+    }
+
+    this.schedule.id = uuid();
+    this.scheduleService.store(this.schedule);
+    this.goHome();
+  }
+  
+  public goHome(){
+    this.scheduleService.navigate('/home');
+  }
 }
