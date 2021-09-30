3bd0366154d8ff0a5bcf819eafe2c9561cf7fe41
Tiagooow <tiagobianezi.tb@gmail.com>
2021-09-29 21:19:52 -0300

    refactor: add property image

--- src/app/services/schedule.service.ts
+++ src/app/services/schedule.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { format } from 'date-fns';
 
-export interface ISchedule{
+export interface ISchedule {
   id: string;
   day: string;
   hour: string;
@@ -12,36 +12,45 @@ export interface ISchedule{
 
 export interface IBarber {
   name: string;
+  image: string;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ScheduleService {
   private barbers: IBarber[] = [
     {
-      name: 'João Gabriel'
+      name: 'João Gabriel',
+      image:
+        'https://avatars.githubusercontent.com/u/56719166?s=400&u=d8a94923f3a5d485110158976b1bae998ed3f26c&v=4',
     },
     {
-      name: 'Felippe dos Santos'
+      name: 'Felippe dos Santos',
+      image:
+        'https://avatars.githubusercontent.com/u/75647162?s=400&u=82a9ba0605de95a96bed703fa841408a4eea3913&v=4',
     },
     {
-      name: 'Tiago Turola'
+      name: 'Tiago Turola',
+      image:
+        // eslint-disable-next-line max-len
+        'https://media-exp1.licdn.com/dms/image/C4D03AQESo6ecF3uOAg/profile-displayphoto-shrink_800_800/0/1582833096300?e=1638403200&v=beta&t=uQBzn1uAY6xEFAknI6xcyNdiXTyxyyOj8EYAQ3REJ2o',
     },
     {
-      name: 'Bruno'
-    }
+      name: 'Bruno',
+      image: '',
+    },
   ];
   private schedules: ISchedule[] = [];
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
-  public init(){
+  public init() {
     return {
       id: '',
       day: '',
       hour: '',
       barberName: '',
-      userId: ''
+      userId: '',
     };
   }
 
@@ -52,7 +61,9 @@ export class ScheduleService {
       return this.schedules;
     }
 
-    const indexSchedules = this.schedules.findIndex((schedule) => schedule.id === scheduleId );
+    const indexSchedules = this.schedules.findIndex(
+      (schedule) => schedule.id === scheduleId
+    );
 
     if (indexSchedules > -1) {
       this.schedules.splice(indexSchedules, 1);
@@ -66,14 +77,19 @@ export class ScheduleService {
 
   public store(schedule: ISchedule) {
     schedule.day = format(new Date(schedule.day), 'dd/MM/yyyy');
-
     this.schedules.push(schedule);
+
+    console.log(schedule);
 
     return this.schedules;
   }
 
   public findAll() {
     return this.schedules;
+  }
+
+  public async findAllScheduleByLoggedUser(userId: string) {
+    return this.schedules.filter((schedule) => schedule.userId === userId);
   }
 
   public findAllBarbers() {
